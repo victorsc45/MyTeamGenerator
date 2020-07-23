b84f45569f470c1eb5e7badadfa381f527444cec
victorsc45 <victorlitzau@gmail.com>
2020-07-23 16:15:08 -0400

    update engineer function gethub

--- app.js
+++ app.js
@@ -5,30 +5,16 @@ const render = require("./lib/htmlRenderer");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-//const util = require("util");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-const teamMembers = [];
-
-// const allnumeric = async (id) => {
-//     var nums = /^[0-9]+$/;
-//     if (id.value.match(nums)) {
-//         console.log('Your Employee number has accepted....');
-
-//         return true;
-//     }
-//     else {
-//         console.log('Please input numeric characters only');
-
-//         return false;
-//     }
-// }
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-//const writeFileAsync = util.promisify(fs.writeFile);
+const employees = [];
 
 // function call to initialize program
+
 promptUser();
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
 // array of questions for user using inquirer node module functionality of inquirer to prompt user
 
 async function promptUser() {
@@ -43,21 +29,17 @@ async function promptUser() {
             type: "input",
             name: "id",
             message: "Please enter employee id whole numbers only:",
-            // validate: function (id) {
-            //     let nums = /^[0-9]+$/;
-            //     if (id.value.match(nums)) {
-            //         console.log('Your Employee number has accepted....');
-
-            //         return true;
-            //     }
-            //     else {
-            //         console.log('Please input numeric characters only');
-
-            //         return false;
-            //     }
-            // }
+            validate: function (id) {
+                idValid = /^[0-9]+$/.test(id)
+                if (idValid) {
+                    console.log("      Awesome, Thanks")
+                    return true;
+                } else {
+                    console.log(".  Please enter a valid whole number for id")
+                    return false;
+                }
+            }
         });
-
         const { email } = await inquirer.prompt({
             type: "input",
             name: "email",
@@ -86,20 +68,12 @@ async function promptUser() {
                 "Manager"],
 
         });
-
-
-
         const { empSpecEng } = await inquirer.prompt({
             type: "input",
             name: "empSpecEng",
             message: "As an engineer enter the Github username:",
             when: (answers) => role === "Engineer",
         })
-
-
-
-
-
         const { empSpecIntrn } = await inquirer.prompt({
             type: "input",
             name: "empSpecIntrn",
@@ -107,63 +81,57 @@ async function promptUser() {
             when: (answers) => role === "Intern",
 
         })
-
-
-
         const { empSpecMngr } = await inquirer.prompt({
             type: "input",
             name: "empSpecMngr",
             message: "Enter Manager's office space number:",
             when: (answers) => role === "Manager",
         })
-
+        // After the user has input all employees desired, call the `render` function (required
+        // above) and pass in an array containing all employee objects; the `render` function will
+        // generate and return a block of HTML including templated divs for each employee!
         switch (role) {
             case "Engineer":
                 let github = empSpecEng;
-                teamMembers.push(new Engineer(name, id, email, github));
+                employees.push(new Engineer(name, id, email, github));
                 console.log("engineer is working");
                 break;
             case "Intern":
                 let school = empSpecIntrn;
-                teamMembers.push(new Intern(name, id, email, school));
+                employees.push(new Intern(name, id, email, school));
                 console.log("intern is working");
                 break;
             case "Manager":
                 let officeNumber = empSpecMngr;
-                teamMembers.push(new Manager(name, id, email, officeNumber));
+                employees.push(new Manager(name, id, email, officeNumber));
                 console.log("manager is working");
                 break;
         }
-
-
-
         const { addTeamMember } = await inquirer.prompt({
             type: "list",
             message: "Add another Team Member?",
             name: "addTeamMember",
             choices: ["Yes", "No"],
-
-
         })
+
+
+        // After you have your html, you're now ready to create an HTML file using the HTML
+        // returned from the `render` function. Now write it to a file named `team.html` in the
+        // `output` folder.
         let addMem = addTeamMember;
         switch (addMem) {
             case "Yes":
-                console.log("team right now:", teamMembers);
+                console.log("team right now:", employees);
                 console.log("Successfully input employee / add new member" + addMem);
                 promptUser();
                 break;
             case "No":
                 console.log("addtoteamFalse", addMem);
-                console.log("team:", teamMembers);
-                //  const html = render(teamMembers);
-                // fs.writeFileSync(outputPath, html, "utf8");
+                console.log("team:", employees);
+                const html = render(employees);
+                fs.writeFileSync(outputPath, html, "utf8");
                 break;
         }
-
-
-
-
-
     } catch (err) {
         console.log(err);
     }
@@ -171,13 +139,9 @@ async function promptUser() {
 
 
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
+
+//You can use the variable`outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
 // const OUTPUT_DIR = path.resolve(__dirname, "output");
@@ -185,9 +149,7 @@ async function promptUser() {
 
 // const render = require("./lib/htmlRenderer");
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+
 
 // HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
 // and Intern classes should all extend from a class named Employee; see the directions
