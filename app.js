927c2862ff619c7b93902081250830280f950691
victorsc45 <victorlitzau@gmail.com>
2020-07-24 12:45:46 -0400

    app.js is working in node js and output dir and team html is generating properly

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// required libs and node packages
+
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
@@ -5,8 +7,14 @@ const render = require("./lib/htmlRenderer");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+
+// output directory and final html team view
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+// array to store created team member json objects
+
 const employees = [];
 
 // function call to initialize program
@@ -15,35 +23,44 @@ promptUser();
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
-// array of questions for user using inquirer node module functionality of inquirer to prompt user
 
 async function promptUser() {
     try {
         console.log("Welcome to the CLI Team Generator add your team members here!");
+
+        // create name of each employee
+
         const { name } = await inquirer.prompt({
             type: "input",
             name: "name",
-            message: "Please enter the employee name here:"
+            message: "Employee's name:  "
         });
+
+        // create id for each employee validate as whole number
+
         const { id } = await inquirer.prompt({
             type: "input",
             name: "id",
-            message: "Please enter employee id whole numbers only:",
+            message: "Employee's ID:  ",
             validate: function (id) {
-                idValid = /^[0-9]+$/.test(id)
+                const idValid = /^[0-9]+$/.test(id)
                 if (idValid) {
                     console.log("      Awesome, Thanks")
                     return true;
-                } else {
-                    console.log(".  Please enter a valid whole number for id")
+                }
+                else {
+                    console.log(".  Please enter a new id; not a valid whole number")
                     return false;
                 }
             }
         });
+
+        // create email for each employee validate as email type
+
         const { email } = await inquirer.prompt({
             type: "input",
             name: "email",
-            message: "Please enter employee's email",
+            message: "Employee's email:  ",
             default: () => { },
             validate: function (email) {
 
@@ -58,6 +75,9 @@ async function promptUser() {
                 }
             }
         });
+
+        // select role of employee being entered on CLI 
+
         const { role } = await inquirer.prompt({
             type: "list",
             name: "role",
@@ -68,12 +88,18 @@ async function promptUser() {
                 "Manager"],
 
         });
+
+        // unique identifier input for Engineer members based on answer of role prompt using when: conditional
+
         const { empSpecEng } = await inquirer.prompt({
             type: "input",
             name: "empSpecEng",
             message: "As an engineer enter the Github username:",
             when: (answers) => role === "Engineer",
         })
+
+        // unique identifier input for Inter members based on answer of role prompt using when: conditional
+
         const { empSpecIntrn } = await inquirer.prompt({
             type: "input",
             name: "empSpecIntrn",
@@ -81,15 +107,18 @@ async function promptUser() {
             when: (answers) => role === "Intern",
 
         })
+
+        // unique identifier input for Manager members based on answer of role prompt using when: conditional
+
         const { empSpecMngr } = await inquirer.prompt({
             type: "input",
             name: "empSpecMngr",
             message: "Enter Manager's office space number:",
             when: (answers) => role === "Manager",
         })
-        // After the user has input all employees desired, call the `render` function (required
-        // above) and pass in an array containing all employee objects; the `render` function will
-        // generate and return a block of HTML including templated divs for each employee!
+
+        // switch case used to push Engineer, Intern, and Manager json object to employees array
+
         switch (role) {
             case "Engineer":
                 let github = empSpecEng;
@@ -107,6 +136,9 @@ async function promptUser() {
                 console.log("manager is working");
                 break;
         }
+
+        // inquirer if there are any more employee enteries at this time 
+
         const { addTeamMember } = await inquirer.prompt({
             type: "list",
             message: "Add another Team Member?",
@@ -115,9 +147,9 @@ async function promptUser() {
         })
 
 
-        // After you have your html, you're now ready to create an HTML file using the HTML
-        // returned from the `render` function. Now write it to a file named `team.html` in the
-        // `output` folder.
+        //  create an HTML file using the HTML
+        // returned from the `render` function. Now write it to a file named `team.html` in the output folder
+
         let addMem = addTeamMember;
         switch (addMem) {
             case "Yes":
@@ -128,14 +160,12 @@ async function promptUser() {
             case "No":
                 console.log("addtoteamFalse", addMem);
                 console.log("team:", employees);
-                // const html = render(employees);
-                //console.log("html", html);
                 if (!fs.existsSync(OUTPUT_DIR)) { fs.mkdirSync(OUTPUT_DIR) };
-                fs.writeFileSync(outputPath, render(employees), "utf-8");
+                fs.writeFileSync(outputPath, render(employees), "utf8");
                 break;
         }
     } catch (err) {
-        console.log(err);
+        console.log(err); // log error if try does not complete
     }
 }
 
@@ -143,18 +173,3 @@ async function promptUser() {
 
 
 
-//You can use the variable`outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-// const render = require("./lib/htmlRenderer");
-
-
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
